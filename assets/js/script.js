45d48314f805d98a663e558d210b39c069c5708d
Will <wrightw404@gmail.com>
2022-09-07 21:27:48 -0700

    re-structured my array of question to make it easier to access. updated functions so they matched with new names

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,46 +4,33 @@ var timeSpot = document.querySelector('#timer');
 var questionSpot = document.querySelector('#question');
 var questionBtns = document.querySelector('#questionButtons');
 var randomQuestion;
-var questionI;
+var questionI = 0;
 //var questionBoxEl = $('#questionBox');
 //var hideQuestions = document.getElementsByClassName('hidden');
 
-var questionArr = [
+var questions = [
     {
-        question: "JavaScript is a ___ -side programming language.",
-        answers: [ 
-            {text: "client", correct: true},
-            {text: "server", correct: false},
-            {text: "both", correct: false},
-            {text: "none", correct: false}
-        ]
+        title: "JavaScript is a ___ -side programming language.",
+        options: ["client", "server", "both", "neither"],
+        answers: "server"
+        
     }, 
     {
-        question:"Inside which HTML element do we put the JavaScript?",
-        answers: [ 
-            {text: "<scripting>", correct: false},
-            {text: "<javascript>", correct: false},
-            {text: "<script>", correct: true},
-            {text: "<js>", correct: false}
-        ]
+        title:"Inside which HTML element do we put the JavaScript?",
+        options: ["<scripting>", "<javascript>", "<script>", "<js>"],
+        answers: "<script>"
     },
     {
-        question: "Where is the correct place to insert a JavaScript?",
-        answers: [ 
-            {text: "<head> section", correct: false},
-            {text: "both <head> & <body>", correct: false},
-            {text: "<body> section", correct: true},
-            {text: "neither", correct: false}
-        ]
+        title: "Where is the correct place to insert a JavaScript?",
+        options: ["<head> section", "both <head> & <body>", "<body> section", "neither"],
+        answers: "<body> section"
+        
     },
     {
-        question: "What is the correct syntax for referring to an external script called?",
-        answers: [ 
-            {text: "<script name=''>", correct: false},
-            {text: "<script src=''>", correct: true},
-            {text: "<script href=''>", correct: false},
-            {text: "<link href=''>", correct: false}
-        ]
+        title: "What is the correct syntax for referring to an external script called?",
+        options: ["<script name=''>", "<script src=''>", "<script href=''>", "<link href=''>"],
+        answers: "<script src=''>"
+        
     }
 ]
 
@@ -55,15 +42,12 @@ $('#startBtn').on('click', gameStart)
 function gameStart() {
    questionBoxEl.removeAttribute('class');
     $('#wrapper').remove();
-    randomQuestion = questionArr.sort(() => Math.random() - .5);
+    randomQuestion = questions.sort(() => Math.random() - .5);
     questionI = 0;
 
     startTimer();
 }
 
-function endQuiz() {
-
-}
 
 function startTimer() {
     var timeLeft = 5;
@@ -91,34 +75,60 @@ function startTimer() {
             //when timer ends create function that brings you to scorecard
         }
     }, 1000);
-    nextQuestion();
+    showQuestion();
+}
+
+function showQuestion(){
+    var shownQuestion = questions[questionI];
+    //var questionTitle = document.getElementById('question');
+    questionSpot.textContent = shownQuestion['title'];
+    questionBtns.textContent = " ";
+    for (var i = 0; i < shownQuestion['options'].length; i++){
+    var answerBtns = shownQuestion['options'][i];
+    var newAnswerButtons = document.createElement('button');
+    newAnswerButtons.setAttribute("class", "options");
+    newAnswerButtons.setAttribute("value", answerBtns);
+    newAnswerButtons.textContent = `${i + 1}. ${answerBtns}`
+    questionBtns.appendChild(newAnswerButtons);
+
+    }
+    
 }
 
 function nextQuestion(){
-    showQuestion(randomQuestion[questionI]);
+    
 }
 
-function showQuestion(question){
-    questionSpot.textContent = question.question;
-    question.answers.forEach(answer => {  //=> arrow function
-        const newAnswerButtons = document.createElement('button'); 
-        newAnswerButtons.textContent = answer.text;  //creates buttons with the text from the answers 
-        newAnswerButtons.classList.add('btn'); //gives new button the class of 'btn' so it will be styled the same 
-        if (answer.correct) {
-            newAnswerButtons.dataset.correct = answer.correct;
-        }
-        newAnswerButtons.addEventListener('click', chosenAnswer);
-        questionBtns.appendChild(newAnswerButtons);
+
+
+
+
+
+
+
+
+
+//function showQuestion(question){
+//    questionSpot.textContent = question.question;
+//    question.answers.forEach(answer => {  //=> arrow function
+//        const newAnswerButtons = document.createElement('button'); 
+ //       newAnswerButtons.textContent = answer.text;  //creates buttons with the text from the answers 
+//        newAnswerButtons.classList.add('btn'); //gives new button the class of 'btn' so it will be styled the same 
+//        if (answer.correct) {
+ //           newAnswerButtons.dataset.correct = answer.correct;
+ //       }
+ //       newAnswerButtons.addEventListener('click', chosenAnswer);
+//        questionBtns.appendChild(newAnswerButtons);
         //need to append to #questionButtons 
 
-    })
+ //   })
 
-}
+//}
 
-function chosenAnswer() {
+//function chosenAnswer() {
   //  var userAnswer = e.target;
   //  var userAnserValue = userAnswer.dataset.correct
-}
+//}
 //function selectAnswer 
 //for (var i = 0; i < questionArr.length; i++){
  //   var selectQuestion = questionArr.question[i]; 
